e16e3443320087de3f9428d431a16d8c16125d79
Conrad Rossouw <conrad.rossouw@geckoengage.com>
2023-10-16 09:11:48 +0100

    added contentful

---
+++ src/lib/contentful.ts
@@ -0,0 +1,22 @@
+import contentful from "contentful";
+import type { EntryFieldTypes } from "contentful";
+
+export interface RentingStoryPost {
+    contentTypeId: "rentingStory",
+    fields: {
+      storyDetail: EntryFieldTypes.Text
+      response: EntryFieldTypes.Text,
+      town: EntryFieldTypes.Text,
+      coordinates: EntryFieldTypes.Location,
+      emoji: EntryFieldTypes.Symbol,
+    }
+  }
+
+export const contentfulClient = contentful.createClient({
+  space: import.meta.env.CONTENTFUL_SPACE_ID,
+  accessToken: import.meta.env.DEV
+    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
+    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
+  host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
+});
+
