b6c9bff820e0f1020fcad2ea282f71f392ea1a27
Conrad Rossouw <conrad.rossouw@geckoengage.com>
2023-10-16 19:52:11 +0100

    added middleware

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify(),
+  adapter: netlify({
+    edgeMiddleware: true,
+  }),
   integrations: [react()]
 });
